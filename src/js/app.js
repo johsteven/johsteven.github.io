7ec8c6f5bdb14a9d06d351a78ef70981e84dd77e
johsteven <johsteven222004@gmail.com>
2021-03-06 00:05:49 +0700

    update

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+// helpers
+const elQS = el => document.querySelector(el);
+const elQSA = el => document.querySelectorAll(el);
+const on = (el, event, value) => el.addEventListener(event, value);
+
+// variables
+
+// functions
+
+// main
+window.addEventListener('load', init);
+
+function init() {
+    load();
+    eventListener();
+}
+
+function load() {
+
+}
+
+function eventListener() {
+
+}
